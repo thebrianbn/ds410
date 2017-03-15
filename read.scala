2cde90e55d3e2f4e779260d9f73f18d9c3fbb9a3
thebrianbn <thebrianbn@gmail.com>
2017-03-15 19:10:14 -0400

    Update read.scala

--- read.scala
+++ read.scala
@@ -1,2 +1,14 @@
+/*
+Old reader
 val data = sc.textFile("hdfs:/user/xpl5016/Data/2007/oesm07in4/nat3d_may2007_dl.xls.csv")
 val lines = data.map(line => line.split(",").map(elem => elem.trim)).map(x => x.toList)
+*/
+
+import java.io.StringReader
+import au.com.bytecode.opencsv.CSVReader
+
+val input = sc.textFile("hdfs:/user/xpl5016/Data/2007/oesm07in4/nat3d_may2007_dl.xls.csv")
+val result = input.map{ line =>
+val reader = new CSVReader(new StringReader(line));
+reader.readNext();
+}
