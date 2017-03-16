28c7e5274be04b2ec142b87e97f9fa5a6835bdf0
thebrianbn <thebrianbn@gmail.com>
2017-03-16 18:02:18 -0400

    Update read.scala

--- read.scala
+++ read.scala
@@ -10,6 +10,11 @@ import scala.util.Try
 import java.io.PrintWriter
 import java.io.File
 
+val files = List("hdfs:/user/xpl5016/Data/2007/oesm07in4/nat4d_may2007_dl.xls.csv",
+                 "hdfs:/user/xpl5016/Data/2008/oesm08in4/nat4d_M2008_dl.xls.csv",
+                 "hdfs:/user/xpl5016/Data/2009/oesm09in4/nat3d_M2007_dl.xls.csv",
+                 "
+
 val input = sc.textFile("hdfs:/user/xpl5016/Data/2007/oesm07in4/nat3d_may2007_dl.xls.csv")
 val result = input.map{ line =>
 val reader = new CSVReader(new StringReader(line));
