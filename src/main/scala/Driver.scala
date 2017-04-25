f7bd4f42cb2259262f1af13794f96af15740c220
thebrianbn <thebrianbn@gmail.com>
2017-04-25 13:10:01 -0400

    Update Driver.scala

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -1,34 +1,31 @@
-package milestone3
+;package milestone2
 
-import java.io.PrintWriter
-import java.io.StringReader
-import java.io.File
+import java.io.StringWriter
+import au.com.bytecode.opencsv.CSVWriter
 import java.util.Arrays
-
-import scala.collection.JavaConversions
-import scala.io.Source
-import scala.util.Try
-
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
-import org.apache.spark.storage.StorageLevel
+import scala.collection.JavaConversions
+import scala.io.Source
+import au.com.bytecode.opencsv.CSVReader
+import java.io.File
+import scala.util.Try
+import java.io.StringReader
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.mllib.regression.LinearRegressionModel
 import org.apache.spark.mllib.regression.LinearRegressionWithSGD
 import org.apache.spark.sql.DataFrame
-
-import au.com.bytecode.opencsv.CSVReader
+import java.io.PrintWriter
 
 object Milestones {
     // Application Specific Variables
     private final val SPARK_MASTER = "yarn-client"
-    private final val APPLICATION_NAME = "milestone1"
-    private final val DATASET_PATH_PUBMED = "/tmp/pubmed.csv"
+    private final val APPLICATION_NAME = "milestone2"
 
     // HDFS Configuration Files
     private final val CORE_SITE_CONFIG_PATH = new Path("/usr/hdp/current/hadoop-client/conf/core-site.xml")
@@ -36,7 +33,8 @@ object Milestones {
     final val conf = new SparkConf().setMaster(SPARK_MASTER).setAppName(APPLICATION_NAME)
     final val sc = new SparkContext(conf)
     sc.setLogLevel("WARN")  
-
+	
+	// Find the Distance between a node and cluster center
     def Distance(a:Array[Double], b:Array[Double]) : Double = {
         assert(a.length == b.length, "Distance(): features dim does not match.")
         var dist = 0.0
@@ -46,20 +44,44 @@ object Milestones {
         return math.sqrt(dist)
     }
 	
-    // Analyze the result
+    // Read in the data
     def Reader(file_name:String): org.apache.spark.rdd.RDD[Array[String]] = {
-		// Read in test file
-		val input = sc.textFile(file_name)
-		val result = input.map{ line =>
-			val reader = new CSVReader(
-				new StringReader(line)
-			);
-			reader.readNext();
-		}
-		return result
-		}
+	// Read in test file
+	val input = sc.textFile(file_name)
+	val result = input.map{ line =>
+		val reader = new CSVReader(
+			new StringReader(line)
+		);
+		reader.readNext();
+	}
+	return result
+    }
 	
-    def LR(training:DataFrame): Unit = {
+	def write_occ_avg(file_name:String) : Unit = {
+		val result = Reader(file_name)
+		// Read data into map
+		val raw_data = result.map(x => (x(0), x(1), x(2), x(3), x(4), x(5), x(6), x(7), x(8), x(9), x(10), x(11), x(12), x(13), x(14), x(15), x(16), x(17), x(18), x(19), x(20), x(21)))
+
+		// Clean up dataset so all values in column avg_salary(x._11) and med_salary(x._20) are int
+		val clean = raw_data.filter(x => Try(x._11.toFloat).isSuccess).filter(x => Try(x._20.toFloat).isSuccess)
+
+		// Map reduce with respect to avg sal
+		val occ_avg_sal_pairs = clean.map(x => (x._4, x._11.toFloat))
+		val ind_avg_sal_pairs = clean.map(x => (x._2, x._11.toFloat))
+
+		val occ_by_avg_sal = occ_avg_sal_pairs.reduceByKey((x, y) => ((x + y) / 2)).sortBy(_._2)
+		val ind_by_avg_sal = ind_avg_sal_pairs.reduceByKey((x, y) => ((x + y) / 2)).sortBy(_._2)
+		
+		val file_name_regex = """nat.*_dl""".r
+		val name = file_name_regex.findFirstIn(file_name)
+	
+		occ_by_avg_sal.map{case(a, b) =>
+		var line = a.toString + "\t" + b.toString
+		line
+		}.coalesce(1).saveAsTextFile("hdfs:/user/bbn5024/occ" + name);
+	}
+	
+	def LR(training:DataFrame): Unit = {
         // Load and parse the data
         //   val data = sc.textFile("data/mllib/ridge-data/lpsa.data")
         //    val parsedData = data.map { line =>
@@ -116,8 +138,7 @@ object Milestones {
         // lrModel.save(sc, "scalaLinearRegressionModel")
         // val sameModel = LinearRegressionModel.load(sc, "scalaLinearRegressionWithSGDModel")
     }
-
-    // TODO: fix the paras
+	
     def Clustering(file_name:String) : Unit = {
 		// Read in test file
 		val result = Reader(file_name)
@@ -131,15 +152,15 @@ object Milestones {
 		val occ_avg_sal_pairs = clean.map(x => (x._4, x._11.toFloat))
 		val ind_avg_sal_pairs = clean.map(x => (x._2, x._11.toFloat))
 
-		val occ_by_avg_sal = occ_avg_sal_pairs.reduceByKey((x, y) => ((x + y) / 2)).sortBy(_._2)
-		val ind_by_avg_sal = ind_avg_sal_pairs.reduceByKey((x, y) => ((x + y) / 2)).sortBy(_._2)
+		val occ_by_avg_sal = occ_avg_sal_pairs.reduceByKey((x, y) => ((x + y) / 2))
+		val ind_by_avg_sal = ind_avg_sal_pairs.reduceByKey((x, y) => ((x + y) / 2))
 
 		// Map reduce with respect to med sal
 		val occ_med_sal_pairs = clean.map(x => (x._4, x._20.toFloat))
 		val ind_med_sal_pairs = clean.map(x => (x._2, x._20.toFloat))
 		
-		val occ_by_med_sal = occ_med_sal_pairs.reduceByKey((x, y) => ((x + y) / 2)).sortBy(_._2)
-		val ind_by_med_sal = ind_med_sal_pairs.reduceByKey((x, y) => ((x + y) / 2)).sortBy(_._2)     
+		val occ_by_med_sal = occ_med_sal_pairs.reduceByKey((x, y) => ((x + y) / 2))
+		val ind_by_med_sal = ind_med_sal_pairs.reduceByKey((x, y) => ((x + y) / 2))   
 
 		// Occupation 
 		val occ_sorted_a_mean = occ_by_avg_sal.sortBy(_._2)
@@ -168,7 +189,7 @@ object Milestones {
 		var c8 = occ_indexed_med.lookup((occ_list_length_med*0.875).toLong) //cluster center 4
 
 		occ_indexed_med.unpersist()
-		
+
 		// Create cluster centers
 		val occ_clusters = sc.broadcast(Array((0, Array(c1(0).toDouble, c5(0).toDouble)), 
 											  (1, Array(c2(0).toDouble, c6(0).toDouble)), 
@@ -183,7 +204,7 @@ object Milestones {
 		val ind_sorted_a_med = ind_by_med_sal.sortBy(_._2)
 		val ind_list_length_med = ind_sorted_a_med.count()
 		val ind_indexed_med = ind_sorted_a_med.zipWithIndex().map(x => (x._1._2, x._2)).map(x => x.swap) // (index, a_mean)
-		
+
 		ind_indexed_avg.persist(StorageLevel.MEMORY_AND_DISK_SER)
 		
 		// Averages
@@ -202,7 +223,7 @@ object Milestones {
 		c8 = ind_indexed_med.lookup((ind_list_length_med*0.875).toLong) //cluster center 4
 		
 		ind_indexed_med.unpersist()
-		
+
 		// Create cluster centers
 		val ind_clusters = sc.broadcast(Array((0, Array(c1(0).toDouble, c5(0).toDouble)), 
 											  (1, Array(c2(0).toDouble, c6(0).toDouble)), 
@@ -214,27 +235,28 @@ object Milestones {
 		val occ_dist = occ_join.flatMap(samp => occ_clusters.value.map(clus => (samp._1, (clus._1, Distance(samp._2, clus._2)))))
 
 		// Find the nearest cluster center for each node
-		val occ_labels = occ_dist.reduceByKey((a, b) => (if (a._2 > b._2) b; else a)).map(t => (t._1, t._2._1))
+		val occ_labels = occ_dist.reduceByKey((a, b) => (if (a._2 > b._2) b; else a)).map(t => (t._1, t._2._1)).sortBy(_._2)
 		
 		// Find the distance between nodes and cluster centers
 		val ind_join = ind_by_avg_sal.join(ind_by_med_sal).map(x => (x._1, Array(x._2._1.toDouble, x._2._2.toDouble)))        
 		val ind_dist = ind_join.flatMap(samp => ind_clusters.value.map(clus => (samp._1, (clus._1, Distance(samp._2, clus._2))) ))
 
 		// Find the nearest cluster center for each node
-		val ind_labels = ind_dist.reduceByKey((a, b) => (if (a._2 > b._2) b; else a)).map(t => (t._1, t._2._1))
+		val ind_labels = ind_dist.reduceByKey((a, b) => (if (a._2 > b._2) b; else a)).map(t => (t._1, t._2._1)).sortBy(_._2)
 		
 		// Clean up file name
 		val file_name_regex = """nat.*_dl""".r
 		val name = file_name_regex.findFirstIn(file_name)
 		
-		// Print the result with corresponding file name
-		var writer = new PrintWriter(new File("occ"+ name +".txt"))
-		occ_labels.collect().foreach(x => writer.write(x._2 + "\t" + x._1 + "\n"))
-		writer.close()
+		//occ_labels.map{case(a, b) =>
+  //var line = b.toString + "\t" + a.toString
+  //line
+//}.coalesce(1).saveAsTextFile("hdfs:/user/bbn5024/occ_cluster" + name);
 
-		writer = new PrintWriter(new File("ind"+ name +".txt"))
-		ind_labels.collect().foreach(x => writer.write(x._2 + "\t" + x._1 + "\n"))
-		writer.close()
+//ind_labels.map{case(a, b) =>
+ // var line = b.toString + "\t" + a.toString
+ // line
+//}.coalesce(1).saveAsTextFile("hdfs:/user/bbn5024/ind_cluster" + name);
 	}
 
     def main(args: Array[String]): Unit = {
@@ -250,7 +272,7 @@ object Milestones {
         System.out.println("\n----------------------------------------------------------------\n");
 
         //*---- Our Code Begains ----*//
-        val files = List("hdfs:/user/xpl5016/Data/2007/oesm07in4/nat4d_May2007_dl.xls.csv",
+		val files = List("hdfs:/user/xpl5016/Data/2007/oesm07in4/nat4d_May2007_dl.xls.csv",
                         "hdfs:/user/xpl5016/Data/2008/oesm08in4/nat4d_M2008_dl.xls.csv",
                         "hdfs:/user/xpl5016/Data/2009/oesm09in4/nat4d_dl.xls.csv",
                         "hdfs:/user/xpl5016/Data/2010/oesm10in4/nat4d_M2010_dl.xls.csv",
@@ -259,17 +281,10 @@ object Milestones {
                         "hdfs:/user/xpl5016/Data/2013/oesm13in4/nat4d_M2013_dl_1_113300_517100.xls.csv",
                         "hdfs:/user/xpl5016/Data/2014/oesm14in4/nat4d_M2014_dl.xlsx.csv",
                         "hdfs:/user/xpl5016/Data/2015/oesm15in4/nat4d_M2015_dl.xlsx.csv"
-        )
-        
-        val t0 = System.nanoTime()                                      
-        for(file <- files) {
-            Clustering(file)
-        }
-		val t1 = System.nanoTime()
-		println("Elapsed time: " + (t1-t0)/10e9 + "s.")
-
-
-
+        )                                    
+		for(file <- files) {
+			write_occ_avg(file)
+		}
         //*---- Our Code Ends ----*//
     }
 }
